a0b5dc1985b58e0e5ba1b15c5d30290999008e07
Dude699 <113655625+Dude699@users.noreply.github.com>
2022-11-10 10:29:22 +0700

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,62 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Thong tin sinh vien");
+        
+        System.out.println("Nhap ho va ten :");
+        String hoVaTen = sc.nextLine();
+        
+        System.out.println("Nhap ma sinh vien :");
+        Long maSinhVien = sc.nextLong();
+      
+        System.out.println("Nhap cmnd :");
+        Long cmnd = sc.nextLong();
+        
+        System.out.println("Nhap ten lop :");
+        Long tenLop = sc.nextLong();
+        
+        System.out.println("Nhap ten truong :");
+        String tenTruong = sc.nextLine();
+        
+        System.out.println("Nhap diem trung binh :");
+        Float diemTrungBinh = sc.nextFloat();
+        
+        System.out.println("Thong tin cong nhan");
+        
+        System.out.println("Nhap ho va ten :");
+        String tend = sc.nextLine();
+        
+        System.out.println("Nhap dia chi :");
+        String diaChi = sc.nextLine();
+        
+        System.out.println("Nhap so dien thoai :");
+        Long soDienThoai = sc.nextLong();
+        
+        System.out.println("Nhap he so luong :");
+        Long heSoLuong = sc.nextLong();
+        
+        System.out.println("Thong tin xe");
+        
+        System.out.println("Nhap ten xe :");
+        String tenXe = sc.nextLine();
+        
+        System.out.println("Nhap hang xe :");
+        String hangXe = sc.nextLine();
+        
+        System.out.println("Nhap gia :");
+        String gia = sc.nextLine();
+        
+        System.out.println("Nhap mau son :");
+        String mauSon = sc.nextLine();
+        
+        
+	}
+
+}
